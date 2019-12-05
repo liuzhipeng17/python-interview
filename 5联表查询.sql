9c31ad09833ca624e1e616d35e78128cf8d7a2de
liuzhipeng17 <liucpliu@sina.cn>
2019-12-05 17:41:39 +0800

    Update 5联表查询.sql

--- 5联表查询.sql
+++ 5联表查询.sql
@@ -3,9 +3,6 @@ select t1.*, t2.dept_no from salaries as t1 inner join dept_manager as t2 on t1.
 where t1.to_date='9999-01-01'
 and t2.to_date='9999-01-01'
 
-
-
-
 # 使用联合查询
 select t1.*, t2.dept_no from salaries as t1, dept_manager as t2 
 where t1.emp_no=t2.emp_no
