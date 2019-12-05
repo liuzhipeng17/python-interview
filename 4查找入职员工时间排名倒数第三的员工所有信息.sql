02c965539555067ac940fbd9e2bec3d970ccd4f4
liuzhipeng17 <liucpliu@sina.cn>
2019-12-05 16:48:13 +0800

    Update 4查找入职员工时间排名倒数第三的员工所有信息.sql

--- 4查找入职员工时间排名倒数第三的员工所有信息.sql
+++ 4查找入职员工时间排名倒数第三的员工所有信息.sql
@@ -2,7 +2,7 @@ select * from employees as t
 where t.hire_date =(
        select distinct hire_date from employees order by hire_date desc limit 2,1
 )
-
+/*
 LIMIT m,n : 表示从第m+1条开始，取n条数据；
 LIMIT n ： 表示从第0条开始，取n条数据，是limit(0,n)的缩写。
 
@@ -13,4 +13,4 @@ LIMIT n ： 表示从第0条开始，取n条数据，是limit(0,n)的缩写。
         5-21前入职的若干...
 若 不加distinct去重，那么按照日期倒序，limit 2,1（从倒数第2行开始，取一条数据）的查询结果为 5-23
 加了distinct去重，会按入职日期进行分组，多个相同入职日期会分为一组，这样limit 2,1的结果即为 5-21。
-（2）外层的where条件中根据子查询查出的倒数第三晚入职的日期，就能查询出符合条件的员工信息。
+（2）外层的where条件中根据子查询查出的倒数第三晚入职的日期，就能查询出符合条件的员工信息。*/
